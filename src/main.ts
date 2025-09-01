f7b5eab4427f18899557d8b454bd2385e7350b87
juancapera26 <caperajuandavid26@gmail.com>
2025-09-01 14:53:08 -0500

    fix: resolver conflictos en auth.controller y main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,12 +7,12 @@ async function bootstrap() {
   // ✅ Habilitar CORS para local y producción
   app.enableCors({
     origin: [
-      'http://localhost:5173',           // Frontend local (Vite)
+      'http://localhost:5173', // Frontend local (Vite)
       'https://route-manager.vercel.app' // Deploy en Vercel
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+    credentials: true
   });
 
   // ✅ usar puerto dinámico para prod o 3000 en local
