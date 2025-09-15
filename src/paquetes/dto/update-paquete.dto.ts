e0d05d54ede3adbad2da9fb734403fcca532ee5e
master2x <djduarterojas2212@gmail.com>
2025-09-14 21:09:08 -0500

    feat(auth) ajustes de resetpassword para conectar con front

---
+++ src/paquetes/dto/update-paquete.dto.ts
@@ -0,0 +1,6 @@
+// update-paquete.dto.ts
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePaqueteDto } from './create-paquete.dto';
+
+export class UpdatePaqueteDto extends PartialType(CreatePaqueteDto) {}
+
