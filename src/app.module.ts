119e03b3ec54dc336995dbbd6b71a78b38d96396
Jose Becerra <jose@example.com>
2025-10-15 12:37:15 -0500

    commit_final

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 // src/app.module.ts
 import {Module} from '@nestjs/common';
-import {UsersModule} from './interface/controllers/modules/users.module';
-import {AuthModule} from './interface/controllers/modules/auth.module';
-import {UsersModule} from './interface/controllers/modules/users.module';
-import {AuthModule} from './interface/controllers/modules/auth.module';
+
 import {MailModule} from './mail/mail.module';
 import {ManifestsModule} from './interface/controllers/modules/manifests.module';
 import {PaquetesModule} from './interface/controllers/modules/paquetes.module';
-import {NovedadesModule} from './interface/controllers/modules/nes.module';
-import {ConductoresModule} from './interface/controllers/modules/conductores.moduleovedad';
 import {VehiculosModule} from './interface/controllers/modules/vehiculos.module'; // ✅ NUEVO
+import { UsersModule } from './interface/controllers/modules/users.module';
+import { AuthModule } from './interface/controllers/modules/auth.module';
+import { NovedadesModule } from './interface/controllers/modules/novedades.module';
+import { ConductoresModule } from './interface/controllers/modules/conductores.module';
 
 @Module({
   imports: [
